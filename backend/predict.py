648e25517493693db9e6691c4f1d54868bee75d5
MunyinSam <kelvinsam233@gmail.com>
2026-04-12 18:49:48 +0700

    RandomForestRegressor model mock for pm 1

--- backend/predict.py
+++ backend/predict.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from models.pm1_predictor import predict as predict_pm1
+
 
 class Singleton(type):
     _instances = {}
@@ -8,6 +10,8 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__()
         return cls._instances[cls]
-    
-class IndoorPredictor:
-    pass
+
+
+class IndoorPredictor(metaclass=Singleton):
+    def predict_pm1(self, pm1_outdoor: float, windspeed: float, aqi: float, temp_outdoor: float, humid: float) -> float:
+        return predict_pm1(pm1_outdoor, windspeed, aqi, temp_outdoor, humid)
